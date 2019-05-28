08c8fe2b2d40f227d87bcc9e5c0cff20bb9e6345
Ron Schneider <ronschneider@ronschneider-macbookpro4.roam.corp.google.com>
2019-05-28 13:25:57 +0200

    docker stuff

---
+++ docker/build_docker.sh
@@ -0,0 +1,6 @@
+export PROJECT_ID=$(gcloud config list project --format "value(core.project)")
+export IMAGE_REPO_NAME=example_custom_container_image
+export IMAGE_TAG=example_image_tag
+export IMAGE_URI=gcr.io/$PROJECT_ID/$IMAGE_REPO_NAME:$IMAGE_TAG
+
+docker build -f Dockerfile -t $IMAGE_URI ./
