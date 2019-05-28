31264c6d5bd6b13dc9c8c776e905b6c4b148da7d
Ron Schneider <ronschneider@ronschneider-macbookpro4.roam.corp.google.com>
2019-05-28 14:47:21 +0200

    100 `::`

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -3,4 +3,4 @@ export IMAGE_REPO_NAME=example_custom_container_image
 export IMAGE_TAG=example_image_tag
 export IMAGE_URI=gcr.io/$PROJECT_ID/$IMAGE_REPO_NAME:$IMAGE_TAG
 
-docker build -f Dockerfile -t $IMAGE_URI ./
+docker build --no-cache -f Dockerfile -t $IMAGE_URI ./
