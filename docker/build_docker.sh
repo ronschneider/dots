ddac75fc71eeb195acb7fdbc82d89238540d9463
Ron Schneider <ronschneider@ronschneider-macbookpro4.roam.corp.google.com>
2019-05-28 15:20:14 +0200

    cant get push to work

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -3,4 +3,6 @@ export IMAGE_REPO_NAME=example_custom_container_image
 export IMAGE_TAG=example_image_tag
 export IMAGE_URI=gcr.io/$PROJECT_ID/$IMAGE_REPO_NAME:$IMAGE_TAG
 
-docker build --no-cache -f Dockerfile -t $IMAGE_URI ./
+git clone https://github.com/ronschneider/dots.git dots_copy
+docker build -f Dockerfile -t $IMAGE_URI ./
+rm -dfR dots_copy
